9edb2d9e898b97415e3f80896bed11b5aefe16d8
B-Tots <35439143+foo-bbq@users.noreply.github.com>
2020-05-09 13:46:50 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 window.addEventListener('load', function () {
 
-    //path (d) value of moon and sun emoji
-    // the path of sun is morphed to moon and vice versa
+    // Path (d) value of moon and sun emoji
+    // The path of sun is morphed to moon and vice versa
     const moonPath = "M16 27.5C16 42.6878 27.5 52.5 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 16 12.3122 16 27.5Z";
     const sunPath = "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
+    const starPath = ""
 
-    //select svg
+    // Select svg
     const darkMode = document.querySelector("#darkMode");
     //const isn't used because we have to change the value to true later
     let toggle = false;
+    let toggleCount = 0;
+    let blogDisplay = false;
 
-    // We need to click on the sun
+    // Listen for clicks on the sun
     darkMode.addEventListener('click', () => {
-        //We need to animate using anime.js
-        //Here we set the TIMELINE
+        //Animate using anime.js
+        //Here we set the TIMELINE 
         const timeline = anime.timeline({
             duration: 300,
             easing: "easeOutExpo"
         });
         //Add different animations to the timeline
-        timeline.add({
-            targets: ".sun",
-            d: [{ value: toggle ? sunPath : moonPath }]
-        })
+        timeline
+            .add({
+                targets: ".sun",
+                d: [{ value: toggle ? sunPath : moonPath }]
+            })
             .add({
                 targets: "#darkMode",
                 rotate: toggle ? 0 : 320
@@ -34,5 +38,18 @@ window.addEventListener('load', function () {
             }, '-= 150');
         //Everytime we click on the sun toggle switches from true to false
         toggle = !toggle;
+        toggleCount += 1;
+        this.console.log(toggleCount)
+
+        const starTime = anime.timeline({
+            duration: 1000,
+            easing: "linear"
+        })
+        if (toggleCount >= 2) {
+            starTime.add({
+                targets: ".star",
+                opacity: toggle ? '1' : '0',
+            });
+        }
     });
 })
